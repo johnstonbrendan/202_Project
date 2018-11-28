35b719d1201d4c92e5b800392524aaef78a24e33
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2018-11-28 01:03:17 -0500

    Fix error in ode1 and ode2

--- main.m
+++ main.m
@@ -11,22 +11,22 @@ y_velocity = 0; % this is a assumption for now (m/s)
 
 
 syms z(t) x(t) z_velocity(t) x_velocity(t)
-ode1 = diff(z_velocity) == z;
-ode2 = diff(x_velocity) == x;
-ode3 = x_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity...
+ode1 = diff(z) == z_velocity;
+ode2 = diff(x) == x_velocity;
+ode3 = diff(x_velocity) == (x_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity...
         /velocity(x_velocity,y_velocity,z_velocity) - ...
         drag_force(CD0,CDa,alpha(x_velocity,z_velocity),alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity/...
-        velocity(x_velocity,y_velocity,z_velocity) == m*diff(x_velocity);
-ode4 = z_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity...
+        velocity(x_velocity,y_velocity,z_velocity))/m;
+ode4 = diff(z_velocity) == (z_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity...
         /velocity(x_velocity,y_velocity,z_velocity) + ...
         drag_force(CD0,CDa,alpha(x_velocity,z_velocity),alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity/...
-        velocity(x_velocity,y_velocity,z_velocity) - m*g == m*diff(z_velocity);
+        velocity(x_velocity,y_velocity,z_velocity) - m*g)/m;
     
 cond1 = x_velocity(0) == 0;
 cond2 = z_velocity(0) == 2;
 
     
-odes = [ode3;ode4];
+odes = [ode1;ode3;ode4];
 conds = [cond1; cond2];
 S = dsolve(odes,conds);
 [xSol(t),zSol(t)] = dsolve(odes,conds);
