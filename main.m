d445255c10e2f751a340997f8216b0f850dbc6df
Niel Mistry <nielmistry1@gmail.com>
2018-11-28 01:48:02 -0500

    working numerical ode45

--- main.m
+++ main.m
@@ -4,25 +4,26 @@ CL0 = 1.0; %coefficient of lift (0)
 CLa = 1.0; %coefficient of lift (alpha)
 CD0 = 1.0; %coefficient of drag (0)
 CDa = 1.0; %coefficient of drag (alpha);
-alpha_i = 1.0; %alpha(x_velocity,z_velocity) initial (radians)
+alpha_i = 1.0; %alpha(diff(x),diff(z)) initial (radians)
 rho = 1.0; %density of fliud (NEED UNITS)
 r = 1.0; %radius (m)
 y_velocity = 0; % this is a assumption for now (m/s)
 
 
-syms z(t) x(t) z_velocity(t) x_velocity(t) t Y
-ode1 = diff(z) == z_velocity;
-ode2 = diff(x) == x_velocity;
-ode3 = diff(x_velocity) == (x_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity...
-        /velocity(x_velocity,y_velocity,z_velocity) - ...
-        drag_force(CD0,CDa,alpha(x_velocity,z_velocity),alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity/...
-        velocity(x_velocity,y_velocity,z_velocity))/m;
-ode4 = diff(z_velocity) == (z_velocity + lift_force(CL0,CLa,alpha(x_velocity,z_velocity),rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity...
-        /velocity(x_velocity,y_velocity,z_velocity) + ...
-        drag_force(CD0,CDa,alpha(x_velocity,z_velocity),alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity/...
-        velocity(x_velocity,y_velocity,z_velocity) - m*g)/m;
+syms z(t) x(t) t Y
+ode1 = diff(x, 2) == (diff(x) + lift_force(CL0,CLa,alpha(diff(x),diff(z)),rho,r,velocity(diff(x),y_velocity,diff(z)))*diff(z)...
+        /velocity(diff(x),y_velocity,diff(z)) - ...
+        drag_force(CD0,CDa,alpha(diff(x),diff(z)),alpha_i,rho,r,velocity(diff(x),y_velocity,diff(z)))*diff(x)/...
+        velocity(diff(x),y_velocity,diff(z)))/m;
+ode2 = diff(z, 2) == (diff(z) + lift_force(CL0,CLa,alpha(diff(x),diff(z)),rho,r,velocity(diff(x),y_velocity,diff(z)))*diff(x)...
+        /velocity(diff(x),y_velocity,diff(z)) + ...
+        drag_force(CD0,CDa,alpha(diff(x),diff(z)),alpha_i,rho,r,velocity(diff(x),y_velocity,diff(z)))*diff(z)/...
+        velocity(diff(x),y_velocity,diff(z)) - m*g)/m;
   
-[DEVF, Subs] = odeToVectorField(ode1, ode2, ode3, ode4);
-ODEfcn = matlabFunction(DEVF, 'Vars', {t, Y})
-[t, X] = ode45(@(t,Y) ODEfcn(t, Y), [0 200],[0;2;0;0])
+[DEVF, Subs] = odeToVectorField(ode1, ode2);
+ODEfcn = matlabFunction(DEVF, 'Vars', {t, Y});
+[t, X] = ode45(@(t,Y) ODEfcn(t, Y), [0 200],[0;0;0;4]);
+
+plot(t, X)
+
 
