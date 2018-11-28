d8c5abdc03fd3f1ed922eebf8c8b1895329a0e8c
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2018-11-27 22:32:13 -0500

    fix up spelling errors

--- main.m
+++ main.m
@@ -4,20 +4,26 @@ CL0 = 1.0; %coefficient of lift (0)
 CLa = 1.0; %coefficient of lift (alpha)
 CD0 = 1.0; %coefficient of drag (0)
 CDa = 1.0; %coefficient of drag (alpha);
-a = 1.0; %alpha (radians)
+alpha = 1.0; %alpha (radians)
+alpha_i = 1.0; %alpha initial (radians)
 rho = 1.0; %density of fliud (NEED UNITS)
 r = 1.0; %radius (m)
 y_velocity = 0; % this is a assumption for now (m/s)
 
 
-syms z(t),x(t),z_velocity(t),x_velocity(t)
+syms z(t) x(t) z_velocity(t) x_velocity(t)
 ode1 = diff(z_velocity) == z;
-ode2 = diff(x_veloctiy) == x;
-ode3 = x_velocity + lift_force(CL0,CLa,alpha,rho,r,v)*z_velocity...
+ode2 = diff(x_velocity) == x;
+ode3 = x_velocity + lift_force(CL0,CLa,alpha,rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity...
         /velocity(x_velocity,y_velocity,z_velocity) - ...
-        lift_drag(CD0,CDa,alpha,alpha_i,rho,r,v)*x_velocity/...
+        drag_force(CD0,CDa,alpha,alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity/...
         velocity(x_velocity,y_velocity,z_velocity) == m*diff(x_velocity);
-ode4 = z_velocity + lift_force(CL0,CLa,alpha,rho,r,v)*x_velocity...
+ode4 = z_velocity + lift_force(CL0,CLa,alpha,rho,r,velocity(x_velocity,y_velocity,z_velocity))*x_velocity...
         /velocity(x_velocity,y_velocity,z_velocity) + ...
-        lift_drag(CD0,CDa,alpha,alpha_i,rho,r,v)*z_velocity/...
+        drag_force(CD0,CDa,alpha,alpha_i,rho,r,velocity(x_velocity,y_velocity,z_velocity))*z_velocity/...
         velocity(x_velocity,y_velocity,z_velocity) - m*g == m*diff(x_velocity);
+    
+odes = [ode1;ode2;ode3;ode4];
+S = dsolve(odes);
+S.x
+S.z
