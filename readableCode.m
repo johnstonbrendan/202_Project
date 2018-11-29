b30dc1a503ea20a56b7b262f0d65e4b33e42054d
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2018-11-29 01:15:21 -0500

    Add wind speed into equation and correct naming of alpha_i to alpha_0

--- readableCode.m
+++ readableCode.m
@@ -1,6 +1,5 @@
 
 function solver
-    startHeight = 2;
     % time step setup
     tstart = 0;
     tend = 4;
@@ -9,15 +8,18 @@ function solver
     tspan = linspace(tstart, tend, n);
 
     % initial conditions
+    startHeight = 2; %m
+    wind_speed_x = 12;%(m/s)
+    wind_speed_z = -7;%(m/s)
     xInit = [0; 3; 0; 0; startHeight; 0]; %x, vx, y, vy, z, vz, 
     [t, out] = ode45(@discODEs, tspan, xInit)
 
-    x = out(:,1);
-    vx = out(:,2);
+    x = out(:,1) +(t*wind_speed_x);
+    vx = out(:,2) +(wind_speed_x);
     y = out(:,3);
     vy = out(:,4);
-    z = out(:,5);
-    vz = out(:,6);
+    z = out(:,5) +(t*wind_speed_z);
+    vz = out(:,6) +(wind_speed_z);
 
     figure('Name','x z t plot')
     plot3(t,x,z)
@@ -25,10 +27,11 @@ function solver
     ylabel('x')
     zlabel('z')
     zlim([0 startHeight])
+    
     figure('Name','z t plot')
-    plot(t,z)
+    plot(t,vz)
     xlabel('t')
-    ylabel('z')
+    ylabel('vz')
     ylim ([0 startHeight])
     
     figure('Name','x t plot')
@@ -52,7 +55,7 @@ function ddt = discODEs(t, out)
     CLa = 1.4; %coefficient of lift (alpha)
     CD0 = 0.08; %coefficient of drag (0)
     CDa = 2.72; %coefficient of drag (alpha);
-    alpha_i = 0; %alpha(diff(x),diff(z)) initial (radians)
+    alpha_0 = -0.0698; %alpha(diff(x),diff(z)) 0 defined based on physcial aspects of frisbee (radians)
     rho = 1.225; %density of fliud (NEED UNITS)
     r = 0.137; %radius (m)
     pitch = 0;
@@ -70,9 +73,9 @@ function ddt = discODEs(t, out)
     ddt = zeros(size(out));
     ddt(1) = vx;
     ddt(2) = ((lift_force(CL0, CLa, alpha(vx, vz, pitch), rho, r, velocity(vx, y_velocity, vz))*-vz)/velocity(vx, y_velocity, vz)...
-        - drag_force(CD0, CDa, alpha(vx, vz, pitch), alpha_i, rho, r, velocity(vx, y_velocity, vz))*vx/velocity(vx, y_velocity, vz))/m;
+        - drag_force(CD0, CDa, alpha(vx, vz, pitch), alpha_0, rho, r, velocity(vx, y_velocity, vz))*vx/velocity(vx, y_velocity, vz))/m;
     ddt(5) = vz;
     ddt(6) = (lift_force(CL0, CLa, alpha(vx, vz, pitch), rho, r, velocity(vx, y_velocity, vz))*vx/velocity(vx, y_velocity, vz) ...
-            + drag_force(CD0, CDa, alpha(vx, vz, pitch), alpha_i, rho, r, velocity(vx, y_velocity, vz))*-vz/velocity(vx, y_velocity, vz) ...
+            + drag_force(CD0, CDa, alpha(vx, vz, pitch), alpha_0, rho, r, velocity(vx, y_velocity, vz))*-vz/velocity(vx, y_velocity, vz) ...
             - m*g)/m;
 end
