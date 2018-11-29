a5573f59538baf89c935ee619b165846ceda94a4
Niel Mistry <nielmistry1@gmail.com>
2018-11-29 11:39:01 -0500

    Add pitch to distance calculation

--- readableCode.m
+++ readableCode.m
@@ -7,47 +7,32 @@ function solver
     tstep = 0.001;
     n = (tend-tstart)/tstep;
     tspan = linspace(tstart, tend, n);
-
-    % initial conditions
-    xInit = [0; 10; 0; 0; startHeight; 5]; %x, vx, y, vy, z, vz, 
-    [t, out] = ode45(@discODEs, tspan, xInit)
-
-    x = out(:,1);
-    vx = out(:,2);
-    y = out(:,3);
-    vy = out(:,4);
-    z = out(:,5);
-    vz = out(:,6);
-
-    figure('Name','x y z plot')
-    plot3(x, y,z)
-    xlabel('x')
-    ylabel('y')
-    zlabel('z')
-    xlim([0 20])
-    ylim([0 20])
-    zlim([0 20])
-    figure('Name','z t plot')
-    plot(t,z)
-    xlabel('t')
-    ylabel('z')
-    ylim ([0 startHeight])
     
-    figure('Name','x t plot')
-    plot(t,x)
-    xlabel('t')
-    ylabel('x')
-     
-    figure('Name','y t plot')
-    plot(t,y)
-    xlabel('t')
-    ylabel('y')
+    vThrow = 14;
+    distances = [];
+    for pitch = 0:0.0873:pi/2
+         % initial conditions
+        xInit = [0; vThrow*cos(pitch); 0; 0; startHeight; vThrow*sin(pitch)]; %x, vx, y, vy, z, vz, 
+
+
+        [t, out] = ode45(@(t,out) discODEs(t, out, pitch), tspan, xInit)
+
+        x = out(:,1);
+        vx = out(:,2);
+        y = out(:,3);
+        vy = out(:,4);
+        z = out(:,5);
+        vz = out(:,6);
+        
+        distances =[distances; pitch max(z)];
+    end
     
-    
-    %plot stuff here
+    plot(distances(:,1), distances(:,2))
+
+    %showPlots(t,x,y,z,vx,vy,vz,startHeight);
 end
 
-function ddt = discODEs(t, out)
+function ddt = discODEs(t, out, pitch)
     m = 0.175; %mass of frisbee (kg)
     g = 9.81; %gravity m/s^2
     CL0 = 0.15; %coefficient of lift (0)
@@ -57,8 +42,7 @@ function ddt = discODEs(t, out)
     alpha_i = 0; %alpha(diff(x),diff(z)) initial (radians)
     rho = 1.225; %density of fliud (NEED UNITS)
     r = 0.137; %radius (m)
-    pitch = 0;
-    roll = -pi/3;
+    roll = 0;
     y_velocity = 0; % this is a assumption for now (m/s)
     
 
@@ -105,3 +89,31 @@ function ddt = discODEs(t, out)
     ddt(5) = vz;
     ddt(6) = (liftZ + dragZ - m*g)/m;
 end
+
+function showPlots(t, x, y, z, vx, vy, vz, startHeight)
+    figure('Name','x y z plot')
+    plot3(x, y,z)
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    xlim([0 20])
+    ylim([0 20])
+    zlim([0 20])
+    figure('Name','z t plot')
+    plot(t,z)
+    xlabel('t')
+    ylabel('z')
+    ylim ([0 startHeight])
+    
+    figure('Name','x t plot')
+    plot(t,x)
+    xlabel('t')
+    ylabel('x')
+    
+     
+    figure('Name','y t plot')
+    plot(t,y)
+    xlabel('t')
+    ylabel('y')
+end
+    
